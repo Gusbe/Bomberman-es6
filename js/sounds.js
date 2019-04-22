8b71a2b97961afee1f2b039bbfc441fa8d8a4446
Gusbe <gusbertolo@gmail.com>
2019-04-22 18:04:34 +0200

    Sounds.js -> ES6

--- js/sounds.js
+++ js/sounds.js
@@ -1,39 +1,45 @@
-function Sounds(){
+class Sounds {
 
-  this.bomb =  document.createElement("audio");
-  this.bomb.src = ("./sound/sound-bomb.mp3");
+  constructor (){
 
-  this.powerUp =  document.createElement("audio");
-  this.powerUp.src = ("./sound/sound-powerup.wav");
+    this.bomb =  document.createElement("audio");
+    this.bomb.src = ("./sound/sound-bomb.mp3");
 
-  this.dead =  document.createElement("audio");
-  this.dead.src = ("./sound/sound-died.wav");
+    this.powerUp =  document.createElement("audio");
+    this.powerUp.src = ("./sound/sound-powerup.wav");
 
-  this.speed =  document.createElement("audio");
-  this.speed.src = ("./sound/sound-speed.wav");
+    this.dead =  document.createElement("audio");
+    this.dead.src = ("./sound/sound-died.wav");
 
-  this.enemyDies =  document.createElement("audio");
-  this.enemyDies.src = ("./sound/sound-enemyDies.mp3");
+    this.speed =  document.createElement("audio");
+    this.speed.src = ("./sound/sound-speed.wav");
+
+    this.enemyDies =  document.createElement("audio");
+    this.enemyDies.src = ("./sound/sound-enemyDies.mp3");
+    
+    this.plant =  document.createElement("audio");
+    this.plant.src = ("./sound/sound-plant.wav");
+  }
+
+
+  play (event){
+
+    switch (event){
   
-  this.plant =  document.createElement("audio");
-  this.plant.src = ("./sound/sound-plant.wav");
+      case 'explosion': this.bomb.play(); this.bomb.volume = 0.9;
+                        break;
+      case 'powerUp':   this.powerUp.play(); this.powerUp.volume = 0.7;
+                        break;
+      case 'dead':      this.dead.play();
+                        break;
+      case 'speed':     this.speed.play();
+                        break;
+      case 'enemyDies': this.enemyDies.play();
+                        break;
+      case 'plant':     this.plant.play();
+                        break;
+    }   
+  }
+
 }
 
-Sounds.prototype.play = function (event){
-
-  switch (event){
-
-    case 'explosion': this.bomb.play(); this.bomb.volume = 0.9;
-                      break;
-    case 'powerUp':   this.powerUp.play(); this.powerUp.volume = 0.7;
-                      break;
-    case 'dead':      this.dead.play();
-                      break;
-    case 'speed':     this.speed.play();
-                      break;
-    case 'enemyDies': this.enemyDies.play();
-                      break;
-    case 'plant':     this.plant.play();
-                      break;
-  }   
-}
